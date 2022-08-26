a8deffb447bdd39a598d10301f073eaa8625f6c5
ChloChlo <93141020+Chlooow@users.noreply.github.com>
2022-08-26 19:03:00 +0200

    Add files via upload

--- src/datas/alimentList.js
+++ src/datas/alimentList.js
@@ -1,8 +1,87 @@
+// import des images
+import chikwangue from '../assets/chikwangue.png'
+import gingembre from '../assets/gingembre.png'
+import banane from '../assets/bananeplantain.png'
+import ngai from '../assets/ngai.png'
+import palm from '../assets/palmoil.png'
+import mango from '../assets/mango.png'
+import basil from '../assets/basilic.png'
+import aloe from '../assets/aloe.png'
+import yam from '../assets/yam.png'
+
+
+
+
 export const alimentList = [
     {
         name: 'Chikwangue',
 		category: 'classique',
 		id: '1ed',
-		isBestSale: true
+		isBestSale: true,
+        date : 1,
+        countryCode : 243 
+    },
+    {
+        name: 'Tangawisi',
+		category: 'classique',
+		id: '2ab',
+        date : 1,
+        countryCode : 243 
+    },
+    {
+        name: 'Ngai Ngai',
+		category: 'classique',
+		id: '3sd',
+        date : 2,
+        countryCode : 242 
+    },
+    {
+        name: 'Banane plantain',
+		category: 'classique',
+		id: '4kk',
+        isBestSale : true,
+        date : 1,
+        countryCode : 242 
+    },
+    {
+        name: 'huile de palme',
+		category: 'extérieur',
+		id: '5pl',
+        isBestSale : true,
+        date : 2,
+        countryCode : 243 
+    },
+    {
+        name: 'Mango',
+		category: 'extérieur',
+		id: '6uo',
+        date : 1,
+        countryCode : 242
+    },
+    {
+        name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+        isBestSale: false,
+        isSpecialOffer : true;
+        date : 1,
+        countryCode : 243 
+    },
+    {
+        name: 'aloe',
+		category: 'extérieur',
+		id: '8fp',
+        isBestSale : false,
+        isSpecialOffer: true,
+        date : 1,
+        countryCode : 1
+    },
+    {
+        name: 'Igname',
+		category: 'extérieur',
+		id: '9vn',
+        date : 1,
+        countryCode : 243
     }
+
 ]
